a08577520b2ac29471f7321b2f5e3ca8b22de393
Llafore <lh946630@gmail.com>
2025-02-14 17:31:21 -0300

    implementacao do controller do cartorio

--- src/main/java/com/Llafore/SisVotes/controller/CartorioController.java
+++ src/main/java/com/Llafore/SisVotes/controller/CartorioController.java
@@ -1,11 +1,14 @@
 package com.Llafore.SisVotes.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Llafore.SisVotes.entity.CartorioEntity;
@@ -14,16 +17,27 @@ import com.Llafore.SisVotes.repository.CartorioRepository;
 @RestController
 public class CartorioController {
 	@Autowired
-	CartorioRepository repository;
+	private final CartorioRepository repository;
 	
-	@GetMapping("/api/cartorios")
-	public ResponseEntity<List<CartorioEntity> > listCartorios(){
-		List<CartorioEntity> retorno = repository.findAll();
-		if(retorno.isEmpty()) {
-			return ResponseEntity.noContent().build();
-		} else {
-			return ResponseEntity.ok(retorno);
-		}
+	CartorioController(CartorioRepository repository){
+		this.repository = repository;
 	}
 	
+	@GetMapping("/api/cartorios")
+	public List<CartorioEntity> listCartorios(){
+		return repository.findAll();
+	}
+	
+	@PutMapping("/api/cartorios")
+	public CartorioEntity updateCartorioEntity(@RequestBody CartorioEntity newEntity) {
+		return repository.findById(newEntity.getId())
+				.map(employee -> {
+					employee.setNome(newEntity.getNome());
+					employee.setUf(newEntity.getUf());
+					return repository.save(newEntity);
+				})
+				.orElseGet(() -> {
+					return repository.save(newEntity);
+				});
+	}
 }
